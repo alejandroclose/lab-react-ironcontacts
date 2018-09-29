85b6a463ba5d9f68635b020e8fc37a1a7be935ae
alejandroclose@gmail.com <alejandroclose@gmail.com>
2018-09-29 15:31:23 +0200

    iteration 2 done

--- src/components/List.js
+++ src/components/List.js
@@ -1,27 +1,62 @@
 import React, { Component } from "react";
-import contacts from '../contacts.json';
+import contacts from "../contacts.json";
 
 class List extends Component {
+  state = {
+    list: contacts.slice(0, 5),
+
+  };
+
+  randomContact = () => {
+    console.log("random");
+    const {list} = this.state;
+    const randomNum = Math.floor(Math.random() * Math.floor(contacts.length));
+    list.push(contacts[randomNum]);
+    console.log(list)
+    this.setState({
+      list
+    })
+  };
+
   render() {
     return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Picture</th>
-          <th>Name</th>
-          <th>Popularity</th>
-          </tr>
-          { contacts.slice(0,5).map((contact)=>{
-        return <tr key={contact.name}>
-          <td><img src={contact.pictureUrl} className="contact-pic" alt="contact"/></td>
-          <td>{contact.name}</td>
-          <td>{contact.popularity.toFixed(2)}</td>
-          </tr>
-      })}
-      </tbody>
-    </table>
-
-    )}
+      <div>
+        <button
+          onClick={() => {
+            this.randomContact();
+          }}
+        >
+          Add random contact
+        </button>
+        <table>
+          <tbody>
+            <tr>
+              <th>Picture</th>
+              <th>Name</th>
+              <th>Popularity</th>
+            </tr>
+            {
+              (this.state.list.map(contact => {
+                return (
+                  <tr key={contact.name}>
+                    <td>
+                      <img
+                        src={contact.pictureUrl}
+                        className="contact-pic"
+                        alt="contact"
+                      />
+                    </td>
+                    <td>{contact.name}</td>
+                    <td>{contact.popularity.toFixed(2)}</td>
+                  </tr>
+                );
+              }))
+            }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default List;
